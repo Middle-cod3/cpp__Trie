d1b628397abfb7e0bb794da295f76dd9cd85772b
Middle-cod3 <92102240+Middle-cod3@users.noreply.github.com>
2024-07-01 10:20:55 +0530

    Complete String

--- tries.cpp
+++ tries.cpp
@@ -327,215 +327,215 @@ Counters for tracking the number of words that end at the node (‘cntEndWith’
 // Bruteforce ----------->
 // TC :
 // SC :
-struct Node
-{
-    // Array to store
-    // links to child nodes
-    Node *links[26];
-    // Counter for number of
-    // words that end at this node
-    int cntEndWith = 0;
-    // Counter for number of words
-    // that have this node as a prefix
-    int cntPrefix = 0;
+// struct Node
+// {
+//     // Array to store
+//     // links to child nodes
+//     Node *links[26];
+//     // Counter for number of
+//     // words that end at this node
+//     int cntEndWith = 0;
+//     // Counter for number of words
+//     // that have this node as a prefix
+//     int cntPrefix = 0;
 
-    // Function to check if the
-    // node contains a specific key
-    bool containsKey(char ch)
-    {
-        // Check if the link corresponding
-        // to the character exists
-        return (links[ch - 'a'] != NULL);
-    }
+//     // Function to check if the
+//     // node contains a specific key
+//     bool containsKey(char ch)
+//     {
+//         // Check if the link corresponding
+//         // to the character exists
+//         return (links[ch - 'a'] != NULL);
+//     }
 
-    // Function to get the child
-    // node corresponding to a key
-    Node *get(char ch)
-    {
-        // Return the link
-        // corresponding to the character
-        return links[ch - 'a'];
-    }
+//     // Function to get the child
+//     // node corresponding to a key
+//     Node *get(char ch)
+//     {
+//         // Return the link
+//         // corresponding to the character
+//         return links[ch - 'a'];
+//     }
 
-    // Function to insert a child
-    // node with a specific key
-    void put(char ch, Node *node)
-    {
-        // Set the link corresponding to
-        // the character to the provided node
-        links[ch - 'a'] = node;
-    }
+//     // Function to insert a child
+//     // node with a specific key
+//     void put(char ch, Node *node)
+//     {
+//         // Set the link corresponding to
+//         // the character to the provided node
+//         links[ch - 'a'] = node;
+//     }
 
-    // Function to increment the
-    // count of words that end at this node
-    void increaseEnd()
-    {
-        // Increment the counter
-        cntEndWith++;
-    }
+//     // Function to increment the
+//     // count of words that end at this node
+//     void increaseEnd()
+//     {
+//         // Increment the counter
+//         cntEndWith++;
+//     }
 
-    // Function to increment the count of
-    // words that have this node as a prefix
-    void increasePrefix()
-    {
-        // Increment the counter
-        cntPrefix++;
-    }
+//     // Function to increment the count of
+//     // words that have this node as a prefix
+//     void increasePrefix()
+//     {
+//         // Increment the counter
+//         cntPrefix++;
+//     }
 
-    // Function to decrement the count
-    // of words that end at this node
-    void deleteEnd()
-    {
-        // Decrement the counter
-        cntEndWith--;
-    }
+//     // Function to decrement the count
+//     // of words that end at this node
+//     void deleteEnd()
+//     {
+//         // Decrement the counter
+//         cntEndWith--;
+//     }
 
-    // Function to decrement the count of
-    // words that have this node as a prefix
-    void reducePrefix()
-    {
-        // Decrement the counter
-        cntPrefix--;
-    }
-};
+//     // Function to decrement the count of
+//     // words that have this node as a prefix
+//     void reducePrefix()
+//     {
+//         // Decrement the counter
+//         cntPrefix--;
+//     }
+// };
 
-// Define a class for the
-// trie data structure
-class Trie
-{
-private:
-    // Pointer to the
-    // root node of the trie
-    Node *root;
+// // Define a class for the
+// // trie data structure
+// class Trie
+// {
+// private:
+//     // Pointer to the
+//     // root node of the trie
+//     Node *root;
 
-public:
-    // Constructor to initialize
-    // the trie with an empty root node
-    Trie()
-    {
-        // Create a new root node
-        root = new Node();
-    }
+// public:
+//     // Constructor to initialize
+//     // the trie with an empty root node
+//     Trie()
+//     {
+//         // Create a new root node
+//         root = new Node();
+//     }
 
-    // Function to insert
-    // a word into the trie
-    void insert(string word)
-    {
-        // Start from the root node
-        Node *node = root;
-        // Iterate over each
-        // character in the word
-        for (int i = 0; i < word.size(); i++)
-        {
-            // If the character is
-            // not already in the trie
-            if (!node->containsKey(word[i]))
-            {
-                // Create a new node
-                // for the character
-                node->put(word[i], new Node());
-            }
-            // Move to the child node
-            // corresponding to the character
-            node = node->get(word[i]);
-            // Increment the prefix
-            // count for the node
-            node->increasePrefix();
-        }
-        // Increment the end count
-        // for the last node of the word
-        node->increaseEnd();
-    }
+//     // Function to insert
+//     // a word into the trie
+//     void insert(string word)
+//     {
+//         // Start from the root node
+//         Node *node = root;
+//         // Iterate over each
+//         // character in the word
+//         for (int i = 0; i < word.size(); i++)
+//         {
+//             // If the character is
+//             // not already in the trie
+//             if (!node->containsKey(word[i]))
+//             {
+//                 // Create a new node
+//                 // for the character
+//                 node->put(word[i], new Node());
+//             }
+//             // Move to the child node
+//             // corresponding to the character
+//             node = node->get(word[i]);
+//             // Increment the prefix
+//             // count for the node
+//             node->increasePrefix();
+//         }
+//         // Increment the end count
+//         // for the last node of the word
+//         node->increaseEnd();
+//     }
 
-    // Function to count the number
-    // of words equal to a given word
-    int countWordsEqualTo(string word)
-    {
-        // Start from the root node
-        Node *node = root;
-        // Iterate over each character in the word
-        for (int i = 0; i < word.size(); i++)
-        {
-            // If the character is found in the trie
-            if (node->containsKey(word[i]))
-            {
-                // Move to the child node
-                // corresponding to the character
-                node = node->get(word[i]);
-            }
-            else
-            {
-                // Return 0 if the
-                // character is not found
-                return 0;
-            }
-        }
-        // Return the count of
-        // words ending at the node
-        return node->cntEndWith;
-    }
+//     // Function to count the number
+//     // of words equal to a given word
+//     int countWordsEqualTo(string word)
+//     {
+//         // Start from the root node
+//         Node *node = root;
+//         // Iterate over each character in the word
+//         for (int i = 0; i < word.size(); i++)
+//         {
+//             // If the character is found in the trie
+//             if (node->containsKey(word[i]))
+//             {
+//                 // Move to the child node
+//                 // corresponding to the character
+//                 node = node->get(word[i]);
+//             }
+//             else
+//             {
+//                 // Return 0 if the
+//                 // character is not found
+//                 return 0;
+//             }
+//         }
+//         // Return the count of
+//         // words ending at the node
+//         return node->cntEndWith;
+//     }
 
-    // Function to count the number of
-    // words starting with a given prefix
-    int countWordsStartingWith(string word)
-    {
-        // Start from the root node
-        Node *node = root;
-        // Iterate over each character in the prefix
-        for (int i = 0; i < word.size(); i++)
-        {
-            // If the character is found in the trie
-            if (node->containsKey(word[i]))
-            {
-                // Move to the child node
-                // corresponding to the character
-                node = node->get(word[i]);
-            }
-            else
-            {
-                // Return 0 if the
-                // character is not found
-                return 0;
-            }
-        }
-        // Return the count of
-        // words with the prefix
-        return node->cntPrefix;
-    }
+//     // Function to count the number of
+//     // words starting with a given prefix
+//     int countWordsStartingWith(string word)
+//     {
+//         // Start from the root node
+//         Node *node = root;
+//         // Iterate over each character in the prefix
+//         for (int i = 0; i < word.size(); i++)
+//         {
+//             // If the character is found in the trie
+//             if (node->containsKey(word[i]))
+//             {
+//                 // Move to the child node
+//                 // corresponding to the character
+//                 node = node->get(word[i]);
+//             }
+//             else
+//             {
+//                 // Return 0 if the
+//                 // character is not found
+//                 return 0;
+//             }
+//         }
+//         // Return the count of
+//         // words with the prefix
+//         return node->cntPrefix;
+//     }
 
-    // Function to erase a
-    // word from the trie
-    void erase(string word)
-    {
-        // Start from the root node
-        Node *node = root;
-        // Iterate over each
-        // character in the word
-        for (int i = 0; i < word.size(); i++)
-        {
-            // If the character is
-            // found in the trie
-            if (node->containsKey(word[i]))
-            {
-                // Move to the child node
-                // corresponding to the character
-                node = node->get(word[i]);
-                // Decrement the prefix
-                // count for the node
-                node->reducePrefix();
-            }
-            else
-            {
-                // Return if the
-                // character is not found
-                return;
-            }
-        }
-        // Decrement the end count
-        // for the last node of the word
-        node->deleteEnd();
-    }
-};
+//     // Function to erase a
+//     // word from the trie
+//     void erase(string word)
+//     {
+//         // Start from the root node
+//         Node *node = root;
+//         // Iterate over each
+//         // character in the word
+//         for (int i = 0; i < word.size(); i++)
+//         {
+//             // If the character is
+//             // found in the trie
+//             if (node->containsKey(word[i]))
+//             {
+//                 // Move to the child node
+//                 // corresponding to the character
+//                 node = node->get(word[i]);
+//                 // Decrement the prefix
+//                 // count for the node
+//                 node->reducePrefix();
+//             }
+//             else
+//             {
+//                 // Return if the
+//                 // character is not found
+//                 return;
+//             }
+//         }
+//         // Decrement the end count
+//         // for the last node of the word
+//         node->deleteEnd();
+//     }
+// };
 // Better ----------->
 // TC :
 // SC :
@@ -543,13 +543,103 @@ public:
 // TC :
 // SC :
 /*
-3.
-ANS :
+3.  Complete String
+ANS : Ninja developed a love for arrays and strings so this time his teacher gave him an array of strings, ‘A’ of size ‘N’. Each element of this array is a string. The teacher taught Ninja about prefixes in the past, so he wants to test his knowledge.
+A string is called a complete string if every prefix of this string is also present in the array ‘A’. Ninja is challenged to find the longest complete string in the array ‘A’.If there are multiple strings with the same length, return the lexicographically smallest one and if no string exists, return "None".
+Note :
+String ‘P’ is lexicographically smaller than string ‘Q’, if :
+1. There exists some index ‘i’ such that for all ‘j’ < ‘i’ , ‘P[j] = Q[j]’ and ‘P[i] < Q[i]’. E.g. “ninja” < “noder”.
+2. If ‘P’ is a prefix of string ‘Q’, e.g. “code” < “coder”.
 Input :   || Output :
 */
 // Bruteforce ----------->
 // TC :
 // SC :
+
+class Node
+{
+public:
+    Node *links[26];
+    bool flag = false;
+
+    bool containsKey(char c) { return links[c - 'a'] != nullptr; }
+    Node *get(char ch) { return links[ch - 'a']; }
+    void put(char ch, Node *node) { links[ch - 'a'] = node; }
+    void setEnd() { flag = true; }
+    bool isEnd() { return flag; }
+};
+
+class Trie
+{
+private:
+    Node *root;
+
+public:
+    Trie() { root = new Node(); }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+
+    bool checkIfPrefixExists(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+                if (!node->isEnd())
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+string completeString(int n, vector<string> &a)
+{
+    Trie trie;
+    for (const string &it : a)
+    {
+        trie.insert(it);
+    }
+
+    string longest = "";
+    for (const string &it : a)
+    {
+        if (trie.checkIfPrefixExists(it))
+        {
+            if (it.length() > longest.length() ||
+                (it.length() == longest.length() && it < longest))
+            {
+                longest = it;
+            }
+        }
+    }
+
+    if (longest == "")
+    {
+        return "None";
+    }
+    return longest;
+}
 // Better ----------->
 // TC :
 // SC :
@@ -762,21 +852,24 @@ int main()
 
     // cout << "If words is Trie start with Stri: " << (trie.startsWith("striv") ? "True" : "False") << endl;
 
-    Trie trie;
-    trie.insert("apple");
-    trie.insert("app");
-    cout << "Inserting strings 'apple', 'app' into Trie" << endl;
-    cout << "Count Words Equal to 'apple': ";
-    cout << trie.countWordsEqualTo("apple") << endl;
-    cout << "Count Words Starting With 'app': ";
-    cout << trie.countWordsStartingWith("app") << endl;
-    cout << "Erasing word 'app' from trie" << endl;
-    trie.erase("app");
-    cout << "Count Words Equal to 'apple': ";
-    cout << trie.countWordsEqualTo("apple") << endl;
-    cout << "Count Words Starting With 'apple': ";
-    cout << trie.countWordsStartingWith("app") << endl;
+    // Trie trie;
+    // trie.insert("apple");
+    // trie.insert("app");
+    // cout << "Inserting strings 'apple', 'app' into Trie" << endl;
+    // cout << "Count Words Equal to 'apple': ";
+    // cout << trie.countWordsEqualTo("apple") << endl;
+    // cout << "Count Words Starting With 'app': ";
+    // cout << trie.countWordsStartingWith("app") << endl;
+    // cout << "Erasing word 'app' from trie" << endl;
+    // trie.erase("app");
+    // cout << "Count Words Equal to 'apple': ";
+    // cout << trie.countWordsEqualTo("apple") << endl;
+    // cout << "Count Words Starting With 'apple': ";
+    // cout << trie.countWordsStartingWith("app") << endl;
 
+    int n = 4;
+    vector<string> A = {"ab", "abc", "a", "bp"};
+    cout << completeString(n, A) << endl; // Output: "abc"
     //  End code here-------->>
 
     return 0;
